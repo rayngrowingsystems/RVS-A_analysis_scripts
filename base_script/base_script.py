d38f5893fc84d4c1a742b157a8f8bc8cd6108f69
Alexander Kutschera <alexander.kutschera@rayngrowingsystems.com>
2025-03-19 17:01:44 +0100

    fixed bug in default mask

--- base_script/base_script.py
+++ base_script/base_script.py
@@ -55,7 +55,8 @@ def create_mask(settings, mask_preview=True):
 
     # creates mask preview image
     preview_settings = {
-        "overlay_mask": mask_options["overlay_mask"]
+        "overlay_mask": mask_options["overlay_mask"],
+        "output_image": settings["outputImage"]
     }
 
     rayn_utils.create_mask_preview(binary_img, spectral_array.pseudo_rgb, preview_settings, mask_preview)
